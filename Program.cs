7e9b5bd44c44c57cf995ec275580cd14a70ebf03
Jhonatan Augusto Pereira <122567649+vJhonatan@users.noreply.github.com>
2024-11-26 21:42:30 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => {
+        policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                    .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
